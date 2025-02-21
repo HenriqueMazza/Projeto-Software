3e967eee68e3fb09fdf0ae03edd0940e4354c8da
HENRIQUE RODRIGUES MAZZA DO VALLE <henriquerodriguesmazza@gmail.com>
2025-02-21 08:52:47 -0300

    Desafios+

--- Aula2 - Variaveis/src/Desafios/Desafio4.java
+++ Aula2 - Variaveis/src/Desafios/Desafio4.java
@@ -2,10 +2,10 @@ package Desafios;
 
 import java.util.Scanner;
 
-public class Desafio2 {
+public class Desafio4 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite o capital inicial em reais: ");
@@ -17,12 +17,12 @@ public class Desafio2 {
 		System.out.println("Digite a quantidade de meses: ");
 		double t = sc.nextDouble();
 		
+	
+		double J = (C * i * t);
+		double MT = J + C;
 		
-		
-		double M = C * Math.pow((1 + i), t);
-		
-		System.out.println("O montante total após as taxas é de : " + M);
-		
+		System.out.println("A taxa simples vai ser de: " + J + " reais");
+		System.out.println("O montante total vai ser de: " + MT + " reais");
 
 	}
 
