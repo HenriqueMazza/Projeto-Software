30aca78dcc91ca8d6eec42a9b602cc47546d63ba
HENRIQUE RODRIGUES MAZZA DO VALLE <henriquerodriguesmazza@gmail.com>
2025-03-10 09:54:13 -0300

    while

--- Aula4 - Estrutura de repeticao/src/repeticao/LoopWhile.java
+++ Aula4 - Estrutura de repeticao/src/repeticao/LoopWhile.java
@@ -11,33 +11,32 @@ public class LoopWhile {
 		//login e senha
 		//Permita apenas 5 tentativas
 		
-		String loginSalvo = "admin";
-		String senhaSalva = "admin";
-		String loginUser = "";
-		String senhaUser = "";
-		int contador = 0;
-		int maxcontador = 5;
-		
-		while (contador < maxcontador && !(loginUser.equals(loginSalvo)) || !(senhaUser.equals(senhaSalva))) {
-			
-			
-			System.out.println("Informe seu login: ");
-			loginUser = sc.nextLine();
-			
-			System.out.println("Informe sua senha: ");
-			senhaUser = sc.nextLine();
-			
-			contador++;
-		}
-		
-		if (contador > maxcontador) {
-			System.out.println("Login e senha incorretas");
-		} else {
-			System.out.println("Acesso liberado!");
-		}
-		
-
-		sc.close();
-	}
-
+        String loginSalvo = "admin";
+        String senhaSalva = "admin";
+        String loginUser = "";
+        String senhaUser = "";
+        int contador = 0;
+        int maxcontador = 5;
+        
+        while (contador < maxcontador) {
+            System.out.println("Informe seu login: ");
+            loginUser = sc.nextLine();
+            
+            System.out.println("Informe sua senha: ");
+            senhaUser = sc.nextLine();
+            
+            if (loginUser.equals(loginSalvo) && senhaUser.equals(senhaSalva)) {
+                System.out.println("Acesso liberado!");
+                break; // Sai do loop se o login e senha estiverem corretos
+            }
+            
+            contador++;
+            
+            if (contador >= maxcontador) {
+                System.out.println("Número máximo de tentativas atingido. Acesso negado!");
+            }
+        }
+        
+        sc.close();
+    }
 }
