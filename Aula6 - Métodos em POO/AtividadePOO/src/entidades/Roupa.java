b46689a02f51372929519d3c220b6d2147b23356
Henrique Mazza <henriquerodriguesmazza@gmail.com>
2025-04-06 20:40:59 -0300

    encapsulamento

--- Aula6 - Métodos em POO/AtividadePOO/src/entidades/Roupa.java
+++ Aula6 - Métodos em POO/AtividadePOO/src/entidades/Roupa.java
@@ -2,45 +2,88 @@ package entidades;
 
 public class Roupa {
 
-		//atributos da classe
-		public String marca;
-		public String tipo;
-		public String tamanho;
-		public int quantidade;
-		public double valor;
-	
-		public void decrementa(int quantidade) {
-			
-	        if (quantidade > this.quantidade) {
-	            System.out.println("Não é possível remover mais do que o disponível no estoque!");
-	        } else {
-	            this.quantidade -= quantidade;
-	        }
-		}
-		
-		public void incrementa(int quantidade) {
-			
-			this.quantidade += quantidade;
-		}
+    private String marca;
+    private String tipo;
+    private String tamanho;
+    private int quantidade;
+    private double valor;
 
 
-public double valorTotal() {
+    public Roupa(String marca, String tipo, String tamanho, int quantidade, double valor) {
+        this.marca = marca;
+        this.tipo = tipo;
+        this.tamanho = tamanho;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
 
-	double totalestoque = quantidade * valor;
-	return totalestoque;
-	
-}
+    // Getters e Setters
+    public String getMarca() {
+        return marca;
+    }
 
-public String toString() {
-	return "Roupa [marca=" + marca +
-			"\n, tipor=" + tipo +
-			"\n, tamanho=" + tamanho +
-			"\n, quantidade=" + quantidade + 
-			"\n, valor=" + valor +
-			"\n, valor total: R$" + valorTotal();
-}
-	
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
 
+    public void decrementa(int quantidade) {
+        if (quantidade > this.quantidade) {
+            System.out.println("Não é possível remover mais do que o disponível no estoque!");
+        } else {
+            this.quantidade -= quantidade;
+        }
+    }
 
+    public void incrementa(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+
+    public double valorTotal() {
+        return quantidade * valor;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Roupa [marca=" + marca +
+                "\n, tipo=" + tipo +
+                "\n, tamanho=" + tamanho +
+                "\n, quantidade=" + quantidade +
+                "\n, valor=" + valor +
+                "\n, valor total: R$" + valorTotal() + "]";
+    }
 }
--- Aula6 - Métodos em POO/AtividadePOO/src/main/Main.java
+++ Aula6 - Métodos em POO/AtividadePOO/src/main/Main.java
@@ -8,45 +8,52 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
 
         while (true) {
-            Roupa roupa = new Roupa();
-            
             System.out.println("Informe os dados da roupa\n");
-            System.out.println("Marca: ");
-            roupa.marca = sc.nextLine();
-            System.out.println("Tipo: ");
-            roupa.tipo = sc.nextLine();
-            System.out.println("Tamanho: ");
-            roupa.tamanho = sc.nextLine();
-            System.out.println("Quantidade em estoque: ");
-            roupa.quantidade = sc.nextInt();
-            System.out.println("Valor unitário: ");
-            roupa.valor = sc.nextDouble();
-            
-            System.out.println("Informe a quantidade de roupas a ser removida do estoque: ");
+
+            System.out.print("Marca: ");
+            String marca = sc.nextLine();
+
+            System.out.print("Tipo: ");
+            String tipo = sc.nextLine();
+
+            System.out.print("Tamanho: ");
+            String tamanho = sc.nextLine();
+
+            System.out.print("Quantidade em estoque: ");
             int quantidade = sc.nextInt();
-            roupa.decrementa(quantidade);
-            
-            System.out.println("Informe a quantidade de roupas a ser adicionada ao estoque: ");
-            quantidade = sc.nextInt();
-            roupa.incrementa(quantidade);
-            
-            System.out.println("Dados atualizados da roupa: ");
-            System.out.println("Marca: " + roupa.marca + ", Tipo: " + roupa.tipo + ", Tamanho: " + roupa.tamanho + ", Quantidade: " + roupa.quantidade + ", Valor Unitário: " + roupa.valor);
-            System.out.println("Valor total em estoque: " + roupa.valorTotal());
-            
+
+            System.out.print("Valor unitário: ");
+            double valor = sc.nextDouble();
+
+            Roupa roupa = new Roupa(marca, tipo, tamanho, quantidade, valor);
+
+            System.out.print("Informe a quantidade de roupas a ser removida do estoque: ");
+            int quantidadeRemover = sc.nextInt();
+            roupa.decrementa(quantidadeRemover);
+
+            System.out.print("Informe a quantidade de roupas a ser adicionada ao estoque: ");
+            int quantidadeAdicionar = sc.nextInt();
+            roupa.incrementa(quantidadeAdicionar);
+
+            System.out.println("\nDados atualizados da roupa:");
+            System.out.println("Marca: " + roupa.getMarca() + 
+                               ", Tipo: " + roupa.getTipo() + 
+                               ", Tamanho: " + roupa.getTamanho() + 
+                               ", Quantidade: " + roupa.getQuantidade() + 
+                               ", Valor Unitário: R$" + roupa.getValor());
+            System.out.println("Valor total em estoque: R$" + roupa.valorTotal());
 
             sc.nextLine();
-            
-
-            System.out.println("Deseja cadastrar outra roupa? (s/n)");
+            System.out.print("\nDeseja cadastrar outra roupa? (s/n): ");
             String resposta = sc.nextLine();
             
             if (resposta.equalsIgnoreCase("n")) {
                 break; 
             }
+
+            System.out.println();
         }
         
         sc.close();
